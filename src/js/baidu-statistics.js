4577c4766bd098393ca09403bc9aaa4dc986ea58
王良 <841369634@qq.com>
2023-06-29 13:55:35 +0800

    调整百度统计的ID。

--- src/js/baidu-statistics.js
+++ src/js/baidu-statistics.js
@@ -3,7 +3,7 @@ if ("localhost" !== location.hostname && "127.0.0.1" !== location.hostname) {
 	(function (w, d) {
 		w._hmt = w._hmt || [];
 		const hm = d.createElement("script");
-		hm.src = "https://hm.baidu.com/hm.js?efa7b04534aa7088ff0e2106757375b3";
+		hm.src = "https://hm.baidu.com/hm.js?30c4961fde49211cdc2889ff31d44738";
 		const s = d.getElementsByTagName("script")[0];
 		s.parentNode.insertBefore(hm, s);
 	})(window, document);
